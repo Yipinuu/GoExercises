ee5b333d2bc8a4fc9327c137053a093a4032c482
Yipinuu <yipinuu@qq.com>
2015-09-16 16:24:55 +0800

    20150901616

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 最近在学习 Go 语言，这是随着书籍做的练习。
 [《Go入门指南》](https://github.com/Unknwon/the-way-to-go_ZH_CN)
+[《神奇的Go语言》](http://wiki.jikexueyuan.com/project/magical-go/)
---
+++ magical-go/add.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(a int, b int) (c int) {
+	c = a + b
+	return c
+}
+
+func main() {
+	c := add(1, 2)
+	fmt.Println(c)
+}
